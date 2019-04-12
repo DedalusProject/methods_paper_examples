da8cd79d3273a5e15d47805c333588e927079b9c
J. S. Oishi <jsoishi@gmail.com>
2019-04-12 18:36:35 -0400

    Added Taylor-Couette Stokes flow example. Not clean, but works.

---
+++ taylor_couette_stokes/low_re_dye.py
@@ -0,0 +1,162 @@
+import dedalus.public as de
+from dedalus.tools  import post
+
+import numpy as np
+import logging
+import time
+
+logger = logging.getLogger(__name__)
+
+# parameters
+nr = 64
+nθ = 64
+R_in = 1
+R_out = 2
+
+ν = 10.
+κ = 0.01
+ampl = 1.
+delta = 0.02
+nrot = 8 # four turns clockwise, four turns counterclockwise
+
+# inner rotation periods
+T_inner = 2*np.pi*R_in/ampl
+
+r = de.Chebyshev('r',nr, interval=[R_in, R_out])
+θ = de.Fourier('θ',nθ)
+domain = de.Domain([θ, r], grid_dtype='float')
+
+# forcing 
+def SquareBoundaryForcing(*args):
+    """square-wave in time forcing for left theta velocity
+
+    """
+    t = args[0].value # time
+    ampl = args[1].value # max velocity
+    delta = args[2].value # arctan width
+    nrot = args[3].value # total number of rotations
+    time_signal = (ampl/np.arctan(1/delta))*np.arctan(np.sin(t/nrot)/delta)
+    return time_signal
+
+def Forcing(*args, domain=domain, F=SquareBoundaryForcing):
+    return de.operators.GeneralFunction(domain, layout='g', func=F, args=args)
+
+de.operators.parseables['Vr'] = Forcing
+
+problem = de.IVP(domain, variables=['u', 'v', 'p', 'ur', 'vr', 'c1', 'c2', 'c3'])#, 'c1r', 'c2r', 'c3r'])
+
+problem.parameters['ν'] = ν
+problem.parameters['κ'] = κ
+problem.parameters['ampl'] = ampl
+problem.parameters['delta'] = delta
+problem.parameters['nrot'] = nrot
+
+# not pre-multiplied...don't use this in an equation!
+problem.substitutions['DivU'] = "ur + u/r + dθ(v)/r"
+# assume pre-multiplication by r*r
+problem.substitutions['Lap_s(f, f_r)'] = "r*r*dr(f_r) + r*f_r + dθ(dθ(f))"
+problem.substitutions['Lap_r'] = "Lap_s(u, ur) - u - 2*dθ(v)"
+problem.substitutions['Lap_θ'] = "Lap_s(v, vr) - v + 2*dθ(u)"
+problem.substitutions['UdotGrad_s(f, f_r)'] = "r*r*u*f_r + r*v*dθ(f)"
+#problem.substitutions['UdotGrad_r'] = "UdotGrad_s(u, ur) - r*v*v"
+#problem.substitutions['UdotGrad_t'] = "UdotGrad_s(v, vr) + r*u*v"
+
+# Stokes Flow
+problem.add_equation('r*r*dr(p) - ν*Lap_r = 0')
+problem.add_equation('r*r*dθ(p) - ν*Lap_θ = 0')
+
+# Incompressibility
+problem.add_equation('r*ur + u + dθ(v) = 0')
+
+# dye equations
+# problem.add_equation('r*r*dt(c1) - κ*Lap_s(c1, c1r) = -UdotGrad_s(c1, c1r)')
+# problem.add_equation('r*r*dt(c2) - κ*Lap_s(c2, c2r) = -UdotGrad_s(c2, c2r)')
+# problem.add_equation('r*r*dt(c3) - κ*Lap_s(c3, c3r) = -UdotGrad_s(c3, c3r)')
+problem.add_equation('r*r*dt(c1) = -r*r*u*dr(c1) - r*v*dθ(c1)')
+problem.add_equation('r*r*dt(c2) = -r*r*u*dr(c2) - r*v*dθ(c2)')
+problem.add_equation('r*r*dt(c3) = -r*r*u*dr(c3) - r*v*dθ(c3)')
+
+# First order
+problem.add_equation('dr(u) - ur = 0')
+problem.add_equation('dr(v) - vr = 0')
+# problem.add_equation('dr(c1) - c1r = 0')
+# problem.add_equation('dr(c2) - c2r = 0')
+# problem.add_equation('dr(c3) - c3r = 0')
+
+# boundary conditions
+#problem.add_bc('left(v) = ampl')
+problem.add_bc('left(v) = left(Vr(t, ampl, delta, nrot))')
+problem.add_bc('right(v) = 0')
+
+problem.add_bc('left(u) = 0')
+problem.add_bc('right(u) = 0', condition="nθ != 0")
+problem.add_bc('right(p) = 0', condition="nθ == 0")
+# problem.add_bc('left(c1) = 0')
+# problem.add_bc('right(c1) = 0')
+# problem.add_bc('left(c2) = 0')
+# problem.add_bc('right(c2) = 0')
+# problem.add_bc('left(c3) = 0')
+# problem.add_bc('right(c3) = 0')
+
+solver = problem.build_solver(de.timesteppers.RK443)
+
+# Initial conditions
+def exp_circle(r,θ, r0, θ0, radius, ampl):
+    logger.info("r0 = {}".format(r0))
+    logger.info("θ0 = {}".format(θ0))
+    xprime = r*np.cos(θ) - r0*np.cos(θ0)
+    yprime = r*np.sin(θ) - r0*np.sin(θ0)
+    rprime2 = xprime**2 + yprime**2
+    
+    return ampl*np.exp(-rprime2/radius**2)
+
+c1 = solver.state['c1']
+c2 = solver.state['c2']
+c3 = solver.state['c3']
+
+r0 = R_in + 0.5*(R_out - R_in) # place dye in middle of channel
+radius = 0.25
+c_ampl = 1.
+c1_angle = 0
+c2_angle = 2*np.pi/3.
+c3_angle = 4*np.pi/3.
+
+θθ, rr = domain.grids(scales=domain.dealias)
+
+for f in [c1, c2, c3]:
+    f.set_scales(domain.dealias, keep_data=False)
+c1['g'] = exp_circle(rr, θθ, r0, c1_angle, radius, c_ampl)
+c2['g'] = exp_circle(rr, θθ, r0, c2_angle, radius, c_ampl)
+c3['g'] = exp_circle(rr, θθ, r0, c3_angle, radius, c_ampl)
+
+# Analysis
+analysis_tasks = []
+check = solver.evaluator.add_file_handler('checkpoints', wall_dt=3540, max_writes=50)
+check.add_system(solver.state)
+analysis_tasks.append(check)
+
+snap = solver.evaluator.add_file_handler('snapshots', sim_dt=T_inner/100, max_writes=50)
+snap.add_task('c1', scales=10)
+snap.add_task('c2', scales=10)
+snap.add_task('c3', scales=10)
+snap.add_task('v')
+analysis_tasks.append(snap)
+
+# run control
+solver.stop_wall_time = np.inf
+solver.stop_iteration = np.inf
+solver.stop_sim_time = nrot*T_inner
+dt = 0.01
+
+start  = time.time()
+while solver.ok:
+    if (solver.iteration-1) % 10 == 0:
+        logger.info("Step {:d}; Time = {:e}".format(solver.iteration, solver.sim_time))
+    solver.step(dt)
+stop = time.time()
+
+logger.info("Total Run time: {:5.2f} sec".format(stop-start))
+logger.info('beginning join operation')
+for task in analysis_tasks:
+    logger.info(task.base_path)
+    post.merge_analysis(task.base_path)
---
+++ taylor_couette_stokes/plot_rgb_dye.py
@@ -0,0 +1,82 @@
+"""
+Plot RGB images using three dye fields
+
+Usage:
+    plot_rgb_dye.py <files>... [--output=<dir>]
+
+Options:
+    --output=<dir>  Output directory [default: ./frames]
+
+"""
+from dedalus.extras import plot_tools
+import numpy as np
+import matplotlib.pyplot as plt
+from dedalus.extras import plot_tools
+import h5py
+
+def trim_zero(data):
+    data[data<0] = 0
+    return data
+
+def main(filename, start, count, output):
+    """Save plot of specified tasks for given range of analysis writes."""
+
+    # Plot settings
+    tasks = ['c1', 'c2', 'c3']
+    scale = 2.5
+    dpi = 100
+    savename_func = lambda write: 'write_{:06}.png'.format(write)
+
+    fig = plt.figure()
+    # Plot writes
+    with h5py.File(filename, mode='r') as file:
+        r = file['scales/r/10']
+        theta = file['scales/θ/10']
+
+        theta,r = plot_tools.quad_mesh(theta,r)
+        c1 = file['tasks/c1']
+        c2 = file['tasks/c2']
+        c3 = file['tasks/c3']
+
+        for index in range(start, start+count):
+            c1_mod = c1[index]
+            c1_mod[c1[index]<0] = 0
+            c2_mod = c2[index]
+            c2_mod[c2[index]<0] = 0
+            c3_mod = c3[index]
+            c3_mod[c3[index]<0] = 0
+            colors = tuple(np.array([c1_mod.T.flatten(),c2_mod.T.flatten(),c3_mod.T.flatten()]).transpose().tolist())
+            ax = fig.add_subplot(111,projection='polar')
+            ax.pcolormesh(theta,r,c1[0,:,:].T,color=colors)
+            ax.spines['polar'].set_visible(False)
+
+            ## removing the tick marks
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            # Save figure
+            savename = savename_func(file['scales/write_number'][index])
+            savepath = output.joinpath(savename)
+            fig.savefig(str(savepath), dpi=dpi)
+            fig.clear()
+    plt.close(fig)
+
+
+if __name__ == "__main__":
+
+    import pathlib
+    from docopt import docopt
+    from dedalus.tools import logging
+    from dedalus.tools import post
+    from dedalus.tools.parallel import Sync
+
+    args = docopt(__doc__)
+
+    output_path = pathlib.Path(args['--output']).absolute()
+    # Create output directory if needed
+    with Sync() as sync:
+        if sync.comm.rank == 0:
+            if not output_path.exists():
+                output_path.mkdir()
+    post.visit_writes(args['<files>'], main, output=output_path)
+
