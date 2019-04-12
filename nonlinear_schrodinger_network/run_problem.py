f7427094403e2c07b6342d26c32c5bf0d85bd128
Keaton J. Burns <keaton.burns@gmail.com>
2019-04-12 09:26:25 -0400

    Move NLS problem into main script and make minor modifications

--- nonlinear_schrodinger_network/run_problem.py
+++ nonlinear_schrodinger_network/run_problem.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mpirun -np X python3 nonlinear_schrodinger_network.py
-mpirun -np X python3 -m dedalus merge_procs snapshots
-mpirun -np X python3 -m dedalus merge_procs outputs
+python3 nonlinear_schrodinger_network.py
+mpiexec python3 -m dedalus merge_procs snapshots
+mpiexec python3 -m dedalus merge_procs outputs
