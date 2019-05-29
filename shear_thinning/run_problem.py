2118e7b9dc367bf7f906293fcc654ee42a42eb2b
Keaton J. Burns <keaton.burns@gmail.com>
2019-05-29 11:20:06 -0400

    Rename to match paper sections

--- shear_thinning/run_problem.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mpirun -np X python3 shear_thinning.py
-mpirun -np X python3 -m dedalus merge_procs snapshots
-mpirun -np X python3 -m dedalus merge_procs outputs
--- shear_thinning/shear_thinning.py
+++
@@ -1,9 +0,0 @@
-"""
-Gravity waves
-
-Describe the problem briefly.
-
-It should take approximately X hours on Y (Ivy Bridge/Haswell/Broadwell/Skylake/etc) cores.
-"""
-
-pass
