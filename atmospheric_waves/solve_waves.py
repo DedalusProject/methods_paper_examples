bdcfc806afefab61ab8980b1a8e7a19eb70eb70b
Keaton J. Burns <keaton.burns@gmail.com>
2019-11-14 19:52:10 -0500

    Parallelize atmosphere eigenproblem and try Legendre

--- atmospheric_waves/solve_waves.py
+++ atmospheric_waves/solve_waves.py
@@ -5,24 +5,27 @@ Solves linearized, ideal, fully compressible wave problem to obtain frequencies 
 
 It should take approximately 6 hours on 1 Skylake core.
 """
+
 import numpy as np
 from mpi4py import MPI
 import matplotlib.pyplot as plt
 from dedalus import public as de
 import time
-
+from eigentools import Eigenproblem
+import h5py
 import logging
 logger = logging.getLogger(__name__)
-
 matplotlib_logger = logging.getLogger('matplotlib')
 matplotlib_logger.setLevel(logging.WARNING)
 
-comm = MPI.COMM_WORLD
 
-logger.info("solving for linear, ideal waves, neglecting viscous and thermal diffusion")
-from eigentools import Eigenproblem
-import h5py
+# Parameters
+nz_waves = 192
 
+cw_size = MPI.COMM_WORLD.size
+cw_rank = MPI.COMM_WORLD.rank
+
+# Load background
 atmosphere_file = h5py.File('./atmosphere.h5', 'r')
 z_atmosphere = atmosphere_file['z'][:]
 ln_T = atmosphere_file['ln_T'][:]
@@ -35,10 +38,9 @@ Lz = atmosphere_file['Lz'][()]
 gamma = atmosphere_file['gamma'][()]
 atmosphere_file.close()
 
+# Problem
 gamma = 5/3
-
-nz_waves = 384
-z_basis = de.Chebyshev('z', nz_waves, interval=(0,Lz))
+z_basis = de.Legendre('z', nz_waves, interval=(0, Lz))
 domain_EVP = de.Domain([z_basis], comm=MPI.COMM_SELF)
 waves = de.EVP(domain_EVP, ['u','w','T1','ln_rho1', 'w_z'], eigenvalue='omega')
 n_var = 5
@@ -47,6 +49,7 @@ T0_z = domain_EVP.new_field()
 ln_rho0 = domain_EVP.new_field()
 del_ln_rho0 = domain_EVP.new_field()
 
+# Resample background
 scale = nz/nz_waves
 logger.info("data nz ({:d}) is {} times larger than wave nz ({:d})".format(nz,scale,nz_waves))
 T0.set_scales(scale)
@@ -99,7 +102,11 @@ c_s = np.sqrt(T0['g'][0].real)
 logger.info("max Brunt is |N| = {} and  k_Hρ is {}".format(brunt_max, k_Hρ))
 start_time = time.time()
 EP = Eigenproblem(waves)
-ks = np.logspace(-1,2, num=20)*k_Hρ
+
+# Distribute wavenumbers
+ks = np.logspace(-1, 2, num=20) * k_Hρ
+batch = int(np.ceil(len(ks) / cw_size))
+ks_local = ks[batch*cw_rank:batch*(cw_rank+1)]
 
 freqs = []
 eigenfunctions = {'w':[], 'u':[], 'T':[]}
@@ -111,7 +118,8 @@ rho0['g'] = np.exp(ln_rho0['g'])
 rho0_avg = (rho0.integrate('z')['g'][0]/Lz).real
 logger.debug("aveage ρ0 = {:g}".format(rho0_avg))
 fig, ax = plt.subplots()
-for i, k in enumerate(ks):
+
+for i, k in enumerate(ks_local):
     ω_lamb2 = k**2*c_s2
     ω_plus2 = ω_lamb2 + ω_ac2
     ω_minus2  = brunt2*ω_lamb2/(ω_lamb2 + ω_ac2)
@@ -149,32 +157,43 @@ for i, k in enumerate(ks):
         eigenfunctions['u'][i].append(np.copy(u['g'])/weight)
         eigenfunctions['T'][i].append(np.copy(T['g'])/weight)
 
-
 ax.set_xscale('log')
 end_time = time.time()
 logger.info("time to solve all modes: {:g} seconds".format(end_time-start_time))
 
+# Gather to root node
+def gather_concat(obj):
+    obj = MPI.COMM_WORLD.gather(obj, root=0)
+    if cw_rank == 0:
+        return [item for rank_obj in obj for item in rank_obj]
 
-with h5py.File('wave_frequencies.h5','w') as outfile:
-    scale_group = outfile.create_group('scales')
+freqs = gather_concat(freqs)
+ef_w = gather_concat(eigenfunctions['w'])
+ef_u = gather_concat(eigenfunctions['u'])
+ef_T = gather_concat(eigenfunctions['T'])
+eigenfunctions = {'w': ef_w, 'u': ef_u, 'T': ef_T}
+opm = gather_concat(omega['ω_plus_min'])
+omm = gather_concat(omega['ω_minus_max'])
+omega = {'ω_plus_min': opm, 'ω_minus_max': omm}
+w_weights = gather_concat(w_weights)
 
-    scale_group.create_dataset('grid',data=ks)
-    scale_group.create_dataset('brunt_max', data=brunt_max)
-    scale_group.create_dataset('k_Hρ',  data=k_Hρ)
-    scale_group.create_dataset('c_s',   data=c_s)
-    scale_group.create_dataset('z',   data=z)
-    scale_group.create_dataset('Lz',  data=Lz)
-    scale_group.create_dataset('rho0', data=rho0['g'])
-
-    tasks_group = outfile.create_group('tasks')
-
-    for i, freq in enumerate(freqs):
-        data_group = tasks_group.create_group('k_{:03d}'.format(i))
-        data_group.create_dataset('freq',data=freq)
-        data_group.create_dataset('ω_plus_min',data=omega['ω_plus_min'][i])
-        data_group.create_dataset('ω_minus_max',data=omega['ω_minus_max'][i])
-        data_group.create_dataset('eig_w',data=eigenfunctions['w'][i])
-        data_group.create_dataset('eig_u',data=eigenfunctions['u'][i])
-        data_group.create_dataset('eig_T',data=eigenfunctions['T'][i])
-    outfile.close()
-plt.show()
+# Save data
+if cw_rank == 0:
+    with h5py.File('wave_frequencies.h5','w') as outfile:
+        scale_group = outfile.create_group('scales')
+        scale_group.create_dataset('grid',data=ks)
+        scale_group.create_dataset('brunt_max', data=brunt_max)
+        scale_group.create_dataset('k_Hρ',  data=k_Hρ)
+        scale_group.create_dataset('c_s',   data=c_s)
+        scale_group.create_dataset('z',   data=z)
+        scale_group.create_dataset('Lz',  data=Lz)
+        scale_group.create_dataset('rho0', data=rho0['g'])
+        tasks_group = outfile.create_group('tasks')
+        for i, freq in enumerate(freqs):
+            data_group = tasks_group.create_group('k_{:03d}'.format(i))
+            data_group.create_dataset('freq',data=freq)
+            data_group.create_dataset('ω_plus_min',data=omega['ω_plus_min'][i])
+            data_group.create_dataset('ω_minus_max',data=omega['ω_minus_max'][i])
+            data_group.create_dataset('eig_w',data=eigenfunctions['w'][i])
+            data_group.create_dataset('eig_u',data=eigenfunctions['u'][i])
+            data_group.create_dataset('eig_T',data=eigenfunctions['T'][i])
